6033d2e58ccbd1140645f187f7690dc527b1e32f
Saahil Hakimuddin <saahil@Haxbox.Saahil>
2009-12-21 13:17:30 +0530

    Added the need application, with a keyword search function

--- Alter_Need/need/urls.py
+++ Alter_Need/need/urls.py
@@ -1,19 +1,11 @@
 from django.conf.urls.defaults import *
-from django.contrib.auth.views import logout, login
-from Alter_Need.have.views import profile, register, addLoc, addItem
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-	(r'^have/', include('Alter_Need.have.urls')),
-	(r'^need/', include('Alter_Need.need.urls')),
-	(r'^accounts/login/$', login),
-	(r'^accounts/logout/$', logout, {'next_page': "/have/"}),
-	(r'^accounts/profile/$', profile),
-	(r'^accounts/login/register/$', register),
-    (r'^accounts/profile/addLoc/$', addLoc),
-	(r'^accounts/profile/addItem/$', addItem),
+	(r'^$', 'Alter_Need.need.views.lookup'),
 	# Example:
     # (r'^Alter_Need/', include('Alter_Need.foo.urls')),
 
---
+++ Alter_Need/need/views.py
@@ -0,0 +1,60 @@
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from Alter_Need.have.models import Item, Location
+from googlemaps import GoogleMaps
+import string
+
+def weight_of(text, key):
+	doc_list = string.split(text)
+	key_list = string.split(key)
+
+	tot_weight = 0
+
+	for cur in key_list:
+		if cur in doc_list:
+			tot_weight = tot_weight + len(cur)
+
+	return tot_weight
+
+def search_sort(doc_list, key):
+	found = []
+	for cur in doc_list:
+		if weight_of(cur.description, key) is not 0:
+			if not found:
+				found.append(cur)
+			else:
+				i = len(found) - 1
+				found.append("")
+				while (weight_of(found[i], key) < weight_of(cur, key)) and (i>=0):
+					found[i+1] = found[i]
+					i = i-1
+				found[i+1] = cur;
+	
+	return found
+
+def in_vic(lat1, lng1, lat2, lng2):
+	if (abs(lat1-lat2)<=5.0) and (abs(lng1-lng2)<=5.0):
+		return True
+	else:
+	 	return False
+
+def lookup(request):
+	if (request.method == 'POST'):
+		gmaps = GoogleMaps()
+		latitude, longitude = gmaps.address_to_latlng(request.POST['where'])
+		locs = Location.objects.all()
+		close_by = []
+		for cur in locs:
+			cur_lat = cur.lat
+			cur_lng = cur.lng
+			if in_vic(float(cur_lat), float(cur_lng), latitude, longitude):
+				close_by.append(cur.user)
+
+		items = Item.objects.filter(user__in=close_by)
+		list = search_sort(items, request.POST['what'])
+		return render_to_response("need/lookup.html", {"list": list})
+
+	return render_to_response("need/lookup.html")
